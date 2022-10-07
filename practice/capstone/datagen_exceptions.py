6d10813f3e5f604fff285b608685f80c5b3cbcae
Lastyyy <simekm13@gmail.com>
2022-10-07 14:24:06 +0200

    Created DatagenBaseException for readability

--- practice/capstone/datagen_exceptions.py
+++ practice/capstone/datagen_exceptions.py
@@ -1,52 +1,56 @@
 import logging
 
 
-class InvalidDefaultConfiguration(Exception):
+class DatagenBaseException(Exception):
+    pass
+
+
+class InvalidDefaultConfiguration(DatagenBaseException):
     def __init__(self):
         logging.error('Incorrect type of default value given in the default.ini file. '
                       'For correct types of values for every option check --help')
 
 
-class PathIsFile(Exception):
+class PathIsFile(DatagenBaseException):
     def __init__(self, path):
         logging.error(path + ' is a file, not a directory')
 
 
-class ValueNegative(Exception):
+class ValueNegative(DatagenBaseException):
     def __init__(self, value):
         logging.error(f"{value} can't be negative")
 
 
-class NonexistentSchemaFile(Exception):
+class NonexistentSchemaFile(DatagenBaseException):
     def __init__(self, path):
         logging.error(f"There is no file {path}")
 
 
-class IncorrectSchema(Exception):
+class IncorrectSchema(DatagenBaseException):
     def __init__(self, message):
         logging.error("Given data schema is incorrect: " + message)
 
 
-class WrongTypeRandRange(Exception):
+class WrongTypeRandRange(DatagenBaseException):
     def __init__(self):
         logging.error("'rand()' can be used only with int")
 
 
-class WrongListOfChoices(Exception):
+class WrongListOfChoices(DatagenBaseException):
     def __init__(self, key):
         logging.error(f"List of values for '{key}' is incorrect")
 
 
-class IncorrectType(Exception):
+class IncorrectType(DatagenBaseException):
     def __init__(self, type):
         logging.error(f"Incorrect type given: {type}")
 
 
-class IncorrectValue(Exception):
+class IncorrectValue(DatagenBaseException):
     def __init__(self, value, type):
         logging.error(f"Value {value} is incorrect for a given type - expected {type}")
 
 
-class ForbiddenCharInFileName(Exception):
+class ForbiddenCharInFileName(DatagenBaseException):
     def __init__(self):
         logging.error("'/' character is forbidden in file names in linux systems")
